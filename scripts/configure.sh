3bc13f509f76dc748a79c181dd84b5b272c86532
Steve Gale <steve.gale@computingstudies.net>
2020-09-23 17:27:17 +1000

    test ok

---
+++ scripts/configure.sh
@@ -0,0 +1,4 @@
+sudo apt install default-jre
+sudo apt install python3-pip python3-dev
+python3 -m pip install --upgrade pip
+sudo apt install curl 
---
+++ scripts/downloadDynamoDb.sh
@@ -0,0 +1,2 @@
+curl -O https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.zip
+unzip dynamodb_local_latest.zip
